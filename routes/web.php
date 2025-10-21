0a54cd35832c1b7a10d7afd25f03bf8e292374c6
sebastiancielma <cielmasebastian@gmail.com>
2025-10-22 00:38:14 +0200

    Define $jobs array at the top of web.php

--- routes/web.php
+++ routes/web.php
@@ -2,63 +2,42 @@
 
 use Illuminate\Support\Facades\Route;
 
+$jobs =[ 
+        [
+          'id'=>1,  
+          'title' => 'Senior Laravel Developer',
+          'description' => 'We are looking for a Senior Laravel Developer to join our dynamic team. The ideal candidate will have extensive experience with Laravel and a passion for building scalable web applications.',
+          'location' => 'Remote',
+          'type' => 'Full-time',
+        ],
+        [
+          'id'=>2,  
+          'title' => 'Frontend Engineer',
+          'description' => 'Join our team as a Frontend Engineer and help us create stunning user interfaces. Proficiency in Vue.js and Tailwind CSS is a must.',
+          'location' => 'New York, NY',
+          'type' => 'Contract',
+        ],
+        [
+          'id'=>3,  
+          'title' => 'DevOps Specialist',
+          'description' => 'We are seeking a DevOps Specialist to streamline our deployment processes and ensure the reliability of our infrastructure. Experience with AWS and Docker is required.',
+          'location' => 'San Francisco, CA',
+          'type' => 'Full-time',
+        ]
+       ];
+
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () {
+Route::get('/jobs', function () use ($jobs) {
     return view('jobs', [
-      'jobs' => [
-        [
-          'id'=>1,  
-          'title' => 'Senior Laravel Developer',
-          'description' => 'We are looking for a Senior Laravel Developer to join our dynamic team. The ideal candidate will have extensive experience with Laravel and a passion for building scalable web applications.',
-          'location' => 'Remote',
-          'type' => 'Full-time',
-        ],
-        [
-          'id'=>2,  
-          'title' => 'Frontend Engineer',
-          'description' => 'Join our team as a Frontend Engineer and help us create stunning user interfaces. Proficiency in Vue.js and Tailwind CSS is a must.',
-          'location' => 'New York, NY',
-          'type' => 'Contract',
-        ],
-        [
-          'id'=>3,  
-          'title' => 'DevOps Specialist',
-          'description' => 'We are seeking a DevOps Specialist to streamline our deployment processes and ensure the reliability of our infrastructure. Experience with AWS and Docker is required.',
-          'location' => 'San Francisco, CA',
-          'type' => 'Full-time',
-        ],
-      ]
+      'jobs' => $jobs
 
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-          'id'=>1,  
-          'title' => 'Senior Laravel Developer',
-          'description' => 'We are looking for a Senior Laravel Developer to join our dynamic team. The ideal candidate will have extensive experience with Laravel and a passion for building scalable web applications.',
-          'location' => 'Remote',
-          'type' => 'Full-time',
-        ],
-        [
-          'id'=>2,  
-          'title' => 'Frontend Engineer',
-          'description' => 'Join our team as a Frontend Engineer and help us create stunning user interfaces. Proficiency in Vue.js and Tailwind CSS is a must.',
-          'location' => 'New York, NY',
-          'type' => 'Contract',
-        ],
-        [
-          'id'=>3,  
-          'title' => 'DevOps Specialist',
-          'description' => 'We are seeking a DevOps Specialist to streamline our deployment processes and ensure the reliability of our infrastructure. Experience with AWS and Docker is required.',
-          'location' => 'San Francisco, CA',
-          'type' => 'Full-time',
-        ],
-    ];
+Route::get('/jobs/{id}', function ($id) use ($jobs) {
     
    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
        
