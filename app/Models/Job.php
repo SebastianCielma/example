ee9ca29f4b8639a28aecace140e6b113915ed986
sebastiancielma <cielmasebastian@gmail.com>
2025-10-22 13:07:48 +0200

    Autoloading

---
+++ app/Models/Job.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Models;
+
+class Job {
+    public static function all(): array
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Senior Laravel Developer',
+                'description' => 'We are looking for a Senior Laravel Developer to join our dynamic team. The ideal candidate will have extensive experience with Laravel and a passion for building scalable web applications.',
+                'location' => 'Remote',
+                'type' => 'Full-time',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Frontend Engineer',
+                'description' => 'Join our team as a Frontend Engineer and help us create stunning user interfaces. Proficiency in Vue.js and Tailwind CSS is a must.',
+                'location' => 'New York, NY',
+                'type' => 'Contract',
+            ],
+            [
+                'id' => 3,
+                'title' => 'DevOps Specialist',
+                'description' => 'We are seeking a DevOps Specialist to streamline our deployment processes and ensure the reliability of our infrastructure. Experience with AWS and Docker is required.',
+                'location' => 'San Francisco, CA',
+                'type' => 'Full-time',
+            ],
+        ];
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
-$jobs =[ 
-        [
-          'id'=>1,  
-          'title' => 'Senior Laravel Developer',
-          'description' => 'We are looking for a Senior Laravel Developer to join our dynamic team. The ideal candidate will have extensive experience with Laravel and a passion for building scalable web applications.',
-          'location' => 'Remote',
-          'type' => 'Full-time',
-        ],
-        [
-          'id'=>2,  
-          'title' => 'Frontend Engineer',
-          'description' => 'Join our team as a Frontend Engineer and help us create stunning user interfaces. Proficiency in Vue.js and Tailwind CSS is a must.',
-          'location' => 'New York, NY',
-          'type' => 'Contract',
-        ],
-        [
-          'id'=>3,  
-          'title' => 'DevOps Specialist',
-          'description' => 'We are seeking a DevOps Specialist to streamline our deployment processes and ensure the reliability of our infrastructure. Experience with AWS and Docker is required.',
-          'location' => 'San Francisco, CA',
-          'type' => 'Full-time',
-        ]
-       ];
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () use ($jobs) {
+Route::get('/jobs', function () {
     return view('jobs', [
-      'jobs' => $jobs
+      'jobs' => Job::all()
 
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) use ($jobs) {
+Route::get('/jobs/{id}', function ($id) {
     
-   $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+   $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
        
    return view('job',['job' => $job]); 
 });
